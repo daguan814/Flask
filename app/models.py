8d396e300d7ea49fa2bc6a64234ab72847a20b0f
shuijing <lu873737753@163.com>
2022-05-17 22:44:38 +0800

    盐值哈希加密

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
 from app import login
+
 
 @login.user_loader
 def load_user(Num):
@@ -14,6 +16,7 @@ def load_user(Num):
     else:
         pass
 
+
 class Dept(db.Model):
     # 学院
     DeptNum = db.Column(db.String(4), primary_key=True)
@@ -23,6 +26,7 @@ class Dept(db.Model):
     DeptDesc = db.Column(db.Text)
     teacher = db.relationship('Teacher', backref='dept', lazy='dynamic')
     Majors = db.relationship('Major', backref='dept', lazy='dynamic')
+
 
 class Teacher(UserMixin, db.Model):
     # 教师
@@ -38,10 +42,13 @@ class Teacher(UserMixin, db.Model):
     # override
     def get_id(self):
         return self.TeacherNum
+
     def set_password(self, password):
         self.TeacherNum = generate_password_hash(password)
+
     def check_password(self, password):
         return check_password_hash(self.TeacherPassword, password)
+
 
 class Manager(UserMixin, db.Model):
     # 管理员
@@ -55,10 +62,13 @@ class Manager(UserMixin, db.Model):
     # override
     def get_id(self):
         return self.ManagerNum
+
     def set_password(self, password):
         self.ManagerPassword = generate_password_hash(password)
+
     def check_password(self, password):
         return check_password_hash(self.ManagerPassword, password)
+
 
 class Major(db.Model):
     # 专业
@@ -70,13 +80,17 @@ class Major(db.Model):
     MajorDesc = db.Column(db.Text)
     Students = db.relationship('Student', backref='major', lazy='dynamic')
 
+
 # 选课表
 Course_select_table = db.Table('course_select_table',
-                        db.Column('StudentNum', db.String(8), db.ForeignKey('student.StudentNum'), nullable=False),
-                        db.Column('CourseNum', db.String(10), db.ForeignKey('course.CourseNum'), nullable=False),
-                        db.Column('TeacherNum', db.String(8), db.ForeignKey('teacher.TeacherNum'), nullable=False),
-                        db.Column('Grade', db.Integer)
-                    )
+                               db.Column('StudentNum', db.String(8), db.ForeignKey('student.StudentNum'),
+                                         nullable=False),
+                               db.Column('CourseNum', db.String(10), db.ForeignKey('course.CourseNum'), nullable=False),
+                               db.Column('TeacherNum', db.String(8), db.ForeignKey('teacher.TeacherNum'),
+                                         nullable=False),
+                               db.Column('Grade', db.Integer)
+                               )
+
 
 class Student(UserMixin, db.Model):
     # 学生
@@ -91,10 +105,13 @@ class Student(UserMixin, db.Model):
     # override
     def get_id(self):
         return self.StudentNum
+
     def set_password(self, password):
         self.StudengtPassword = generate_password_hash(password)
+
     def check_password(self, password):
         return check_password_hash(self.StudengtPassword, password)
+
 
 class Course(db.Model):
     # 课程
@@ -105,6 +122,13 @@ class Course(db.Model):
     TeacherNum = db.relationship('Teacher', backref='course', lazy='dynamic')
     CourseDesc = db.Column(db.Text)
 
+
 class TrainingProgram(db.Model):
     # 培养计划
     TPNumber = db.Column(db.String(7), primary_key=True)
+
+# 盐值哈希加密
+
+# xx = "Lhf@2001."
+# shafff = generate_password_hash(xx)
+# print(shafff)
